ebed05ee601b792ecf7fbecfbb3423a0081b0bc3
Joshua <prodmaecro@gmail.com>
2025-02-28 01:26:29 -0500

    font

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -18,7 +18,7 @@ function TaskInput({ addTask }: TaskInputProps) {
     <div className="flex items-center justify-center m-10">
       <div className="relative w-150">
         <input
-          className="w-full h-11 rounded-3xl bg-[#DDDDDD] font-ibm font-medium text-black placeholder-black pl-4 pr-16 outline-0"
+          className="w-full h-11 rounded-3xl bg-[#DDDDDD] font-medium text-black placeholder-black pl-4 pr-16 outline-0"
           type="text"
           id="task"
           placeholder="Enter task here..."
@@ -31,7 +31,7 @@ function TaskInput({ addTask }: TaskInputProps) {
             addTask(inputValue);
             setInputValue('');
           }}
-          className="absolute right-0 top-0 h-11 w-16 rounded-3xl font-ibm font-medium text-white bg-[#c36c6d] outline-[bg-[#c36c6d]
+          className="absolute right-0 top-0 h-11 w-16 rounded-3xl font-medium text-white bg-[#c36c6d] outline-[bg-[#c36c6d]
           hover:bg-[#955354] transform transition-transform duration-100 ease-in-out hover:scale-105 hover:cursor-pointer active:scale-100"
         >
           Add
